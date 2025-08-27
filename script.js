481cc8c202870ab65f0df2b4eecd906dd6a9c62a
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-08-27 14:41:38 +0200

    completamento prezzo base

---
+++ script.js
@@ -0,0 +1,28 @@
+// richiesta di età e chilometri da conseguire
+
+const ageRequest = parseInt(prompt("Quanti anni hai?"));
+
+const kmRequest = parseInt(prompt("Quanti Km fai?"));
+
+// Calcolo prezzo biglietto standard
+
+let kmPrice = 0.21;
+
+let totalPrice = kmRequest * kmPrice;
+
+
+// calcolo prezzo in base all'età
+
+if (ageRequest < 18) {
+
+    (totalPrice * 0.20) / 100
+
+} else if (ageRequest > 65) {
+
+    (totalPrice * 0.40) / 100
+
+};
+
+// prezzo finale 
+
+alert(`Il prezzo totale del biglietto è di €${totalPrice}`);
